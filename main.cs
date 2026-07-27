47e72f5137a4c03c43648818435968d866fabe45
2401_86702825 <2401_86702825@noreply.gitcode.com>
2026-07-27 09:05:06 +0800

    update: 更新文件 main.cs
    Signed-off-by: 2401_86702825 <2401_86702825@noreply.gitcode.com>

--- main.cs
+++ main.cs
@@ -1,9 +1,18 @@
 // -----------------------------------------------------------------------
-// <copyright file="QuernEngine.cs" company="Quern Project">
-//     Copyright (c) 2026 Quern Engine. All rights reserved.
-//     Licensed under the MIT License. See LICENSE file in the project root.
-//     Author: WinXSYPowershell
-//     Bilibili Website: https://space.bilibili.com/3546630315837635?spm_id_from=333.40164.0.0
+// <copyright file="main.cs" company="Quern Project">
+//     Copyright 2026 WinXSYPowershell
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
 // </copyright>
 // -----------------------------------------------------------------------
 using System.Text.RegularExpressions;
